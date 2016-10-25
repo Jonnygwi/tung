f40ad90e35c67aefa92cd6e6726d3266f5d2c5d9
Jonnygwi <jonnygwi@hotmail.co.uk>
2016-10-25 14:08:34 +0100

    Added google analytics

--- js/ui.js
+++ js/ui.js
@@ -3,15 +3,22 @@ $(document).ready(function() {
     $('html, body').animate({
         scrollTop: $("#try").offset().top
     }, 1000);
+    gaSignUp();
   })
   $('#how-button').on('click', function(){
     $('html, body').animate({
         scrollTop: $("#how").offset().top
     }, 1000);
+    gaSignUp();
   })
   $('#what-button').on('click', function(){
     $('html, body').animate({
         scrollTop: $("#what").offset().top
     }, 1000);
+    gaSignUp();
   })
 });
+
+function gaSignUp (){
+  ga('send', 'event', 'SignUp', 'Attempt');
+}
